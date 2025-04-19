eb82bc76f9ae3a71f0606b122069112b804a21e6
sk1a345 <sanjaykohale920@gmail.com>
2025-04-19 11:31:28 +0530

    Visualizing the Linear Regression Model

--- Practice/Simple_Linear_Re.py
+++ Practice/Simple_Linear_Re.py
@@ -22,6 +22,10 @@ print(df.head())
 # Boxplot:
 # sns.boxplot(x="Package",data = df)
 
+
+# Checking whether graph is following linearity or not:
+sns.scatterplot(x='CGPA',y="Package",data =df)
+plt.show()
 # Distribution plot:
 # sns.histplot(df['Package'])
 # plt.show()
@@ -44,6 +48,7 @@ x = df.drop(['Package','Student_ID'],axis=1)
 y = df['Package']
 
 
+
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=1)
 # print(x_train.shape)
 # print(x_test.shape)
@@ -56,6 +61,12 @@ lr = LinearRegression()
 lr.fit(x_train,y_train)
 
 y_pred = lr.predict(x_test)
+# print("Printing predicted value: ",y_pred)
+
+# m = lr.coef_
+# c = lr.intercept_
+# y_pred1 = m*(x_test)+c
+# print("Printing predicted value1: ",y_pred1)
 
 acc = r2_score(y_test,y_pred)
 print("Accuracy score is: ",acc)
@@ -65,3 +76,17 @@ user_input = sc.transform([[user_input]])
 # print(user_input)
 predicted_package = lr.predict(user_input)
 print(f"Predicted Package is: {predicted_package[0]:.2f} LPA")
+
+# sns.scatterplot(x = 'CGPA',y = 'Package',data = df)
+# plt.plot(df['CGPA'],lr.predict(x),c='red')
+# plt.legend(['org data','predict line'])
+# plt.show()
+
+
+plt.scatter(x_test,y_test,color = 'blue',label='Actual Data')
+plt.scatter(x_test,y_pred,color = 'red',label='Predicted Data')
+plt.xlabel("CGPA(SCALED)")
+plt.ylabel("Package")
+plt.title("Actual vs Predicted Package")
+plt.legend()
+plt.show()
