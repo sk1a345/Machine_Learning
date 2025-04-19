5e9cfb47d497398afe713780e370e54d9cea67d5
sk1a345 <sanjaykohale920@gmail.com>
2025-04-19 10:50:20 +0530

    Added practice folder with regression code and dataset

---
+++ Practice/Simple_Linear_Re.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+
+
+df = pd.read_csv("practice/cgpa_package_dataset.csv")
+print(df.head())
+
+# Finding the missing values
+# print(df.describe())
+
+# Finding the null values inside the dataset:
+# print(df.isnull().sum())
+
+# Visualising the outliers
+# Boxplot:
+# sns.boxplot(x="Package",data = df)
+
+# Distribution plot:
+# sns.histplot(df['Package'])
+# plt.show()
+
+sc = StandardScaler()
+df['CGPA'] =sc.fit_transform(df[['CGPA']]) #usually scaling is performed on the input data
+# print(df)
+
+
+# print(df['Package'].mean())
+# print(df['Package'].std())
+
+# print(df.shape)
+df.drop_duplicates(inplace=True)
+# print(df.shape)
+
+# Train-test split:
+
+x = df.drop(['Package','Student_ID'],axis=1)
+y = df['Package']
+
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=1)
+# print(x_train.shape)
+# print(x_test.shape)
+# print(x.shape)
+# print(y.shape)
+# print(y_train.shape)
+# print(y_test.shape)
+
+lr = LinearRegression()
+lr.fit(x_train,y_train)
+
+y_pred = lr.predict(x_test)
+
+acc = r2_score(y_test,y_pred)
+print("Accuracy score is: ",acc)
+
+user_input = float(input("Enter your CGPA score: "))
+user_input = sc.transform([[user_input]])
+# print(user_input)
+predicted_package = lr.predict(user_input)
+print(f"Predicted Package is: {predicted_package[0]:.2f} LPA")
